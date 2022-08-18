6c5e4ec3a9b63aebbcf523944a03aba56b4d238c
gabenn <lukaszcysewski123@gmail.com>
2022-08-18 09:25:46 +0200

    notification instead of modal finishTest function variables names changed

--- resources/js/components/TestComponent.vue
+++ resources/js/components/TestComponent.vue
@@ -2,15 +2,7 @@
     <div class="flex flex-column">
         <notifications position="bottom left" />
         <div v-if="test">
-            <div v-if="!cheat">
-                <!--<ModalComponent
-                    v-if="error"
-                    :error="error"
-                    :message="errorMessage"
-                    :title="errorTitle"
-                    @closeModal="closeModal"
-                >
-                </ModalComponent>-->
+            <div v-if="!didCheat">
                 <div>
                     <h3>Word {{ currentWord }}/{{ wordsArray.length }}</h3>
                     <h5>Max Time: {{maxTime >= 60 
@@ -39,21 +31,27 @@
                     </button>
                 </div>
             </div>
-            <!--<ModalComponent
-                v-else
-                :error="cheat"
-                :message="'Cheating detected. Refresh page to start test again'"
-                :title="'Cheating'"
-                @closeModal="closeModal"
-            ></ModalComponent>-->
+            <div v-else class="h3 m-3 text-center flex justify-content-center flex-column">
+                <p class="text-danger">
+                    You cheated.
+                </p>
+                <div class="h3 m-3 flex justify-content-center">
+                    <button class="btn btn-success m-3" @click="goBack">
+                        Go Back
+                    </button>
+                    <button class="btn btn-success m-3" @click="reRun">
+                        Re-Run
+                    </button>
+                </div>
+            </div>
         </div>
         <div v-else class="flex flex-column justify-content-center">
-            <div class="h3 m-3 flex  justify-content-center text-center flex-column ">
-                <p v-if='pass' class='text-success'>
+            <div class="h3 m-3 flex justify-content-center text-center flex-column ">
+                <p v-if='didPass' class='text-success'>
                     You Passed The Test
                 </p>
                 <p v-else class='text-danger'>
-                    You Failed The Test <br/> {{this.errorMessage}}
+                    You Failed The Test <br/> {{this.scoreMessage}}
                 </p>
                 <p>Your Score: {{ this.good }}/{{ this.wordsArray.length }}</p>
                 <p>Your Time: {{this.totalTime}}seconds</p>
@@ -84,11 +82,8 @@
     </div>
 </template>
 <script>
-import ModalComponent from "./ModalComponent";
-
 export default {
     components: {
-        ModalComponent,
     },
     props: ["course", "words"],
     data() {
@@ -100,33 +95,17 @@ export default {
             wordsArray: [],
             startTime: new Date(),
             totalTime:0,
-            maxTime:0,
-            error: false, 
-            /**
-            * error = {
-            *  title = '',
-            *  message = '',   
-            * }
-            */
-            errorTitle: "",  
-            errorMessage: "",  
-            test: true, // mało czytelna nazwa zmiennej
-            cheat: false, // didCheat
-            pass: false, // didPass?
+            maxTime:0,  
+            scoreMessage: "",  
+            test: true, 
+            didCheat: false, 
+            didPass: false, 
         };
     },
     methods: {
         confirm() {
-            this.validate();
-            // Zamiast if (!this.error) validate może zwracać true/false - jeśli false to walidacja się nie powiodła
-            
-            /**
-            * const didValidate = this.validate();
-            *
-            * if (didValidate) { ... }
-            */
 
-            if (!this.error) {
+            if (this.validate()) {
                 this.currentAnswer = this.currentAnswer.toLowerCase(); 
                 this.answers.push(this.currentAnswer);
                 if (
@@ -143,23 +122,24 @@ export default {
                     this.$refs.answerInput.focus();
                 } else {
                     this.test = false;
-
-                    // wszystko niżej wrzuciłbym do metody finishTest albo calculateScore, czytając co się dzieje przy potwierdzaniu nie muszę znać detali tego jak wynik jest uzyskiwany; robi się też lekki bałagan
-                    this.totalTime = (new Date().getTime() - this.startTime.getTime())/1000
-                    const score = this.good/this.currentWord; 
-                    const passTime = this.totalTime < this.maxTime;
-
-                    if(score > 0.5 && passTime) {
-                        this.pass = true
-                    }
-                    else {
-                        this.pass = false
-                        if(score < 0.5 && !passTime) this.errorMessage = 'Score < 50% And Time > Max Test Time' // Nie powinniśmy raczej traktować słabego wyniku jako error; to nie błąd a informacja
-                        else if(score < 0.5) this.errorMessage = "Score < 50%"
-                        else if(!passTime) this.errorMessage = "Time > Max Test Time"
-                    }
+                    this.finishTest(); 
                 }
             }
+        },
+        finishTest() {
+            this.totalTime = (new Date().getTime() - this.startTime.getTime())/1000
+            const score = this.good/this.currentWord; 
+            const passTime = this.totalTime < this.maxTime
+            if(score > 0.5 && passTime) {
+                this.didPass = true
+            }
+            else {
+                this.didPass = false
+                if(score < 0.5 && !passTime) this.scoreMessage = 'Score < 50% And Time > Max Test Time'
+                else if(score < 0.5) this.scoreMessage = "Score < 50%"
+                else if(!passTime) this.scoreMessage = "Time > Max Test Time"
+            }
+
         },
         goBack() {
             const course = JSON.parse(this.course);
@@ -167,26 +147,18 @@ export default {
         },
         reRun: () => document.location.reload(),
         validate() {
-            // error lepiej jako obiekt
-            if (this.currentAnswer === "") {
-                this.error = true;
-                this.errorTitle = "Answer Input Error";
-                this.errorMessage = "Answer input is empty";
-                
-                this.$notify({text: "Answer input is empty", error: true}); 
-                // return false;
+            if (this.currentAnswer === "") {                
+                this.$notify({text: "Answer input is empty", duration: 4000}); 
+                return false;
             }
 
-            // return true;
-        },
-        closeModal() {
-            this.error = false;
+            return true;
         },
     },
     created() {
         this.wordsArray = JSON.parse(this.words);
 
-        window.addEventListener("blur", (e) => this.cheat = true);
+        window.addEventListener("blur", (e) => this.didCheat = true);
 
         this.maxTime = this.wordsArray.length * 30;
     },
